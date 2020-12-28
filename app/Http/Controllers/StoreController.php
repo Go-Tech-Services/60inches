6d9111e08392eca09e69a62659e280496a434d29
deepalidsp <dipali.kolhe51@gmail.com>
2020-12-28 17:37:35 +0530

    file upload code

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -22,49 +22,33 @@ class StoreController extends Controller
     }
     public function store(Request $request)
     {
-
-    
-
         $request->validate([
             'name' => 'required',
             'store_name' => 'required', 
             'email' => 'required', 
             'store_address' => 'required', 
             'phone' => 'required',
+            'filename' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-    
-        // if(request()->hasFile('filename')){
-        //     $filename=request()->file('filename')->getClientOriginalName();
-        //     $extension = $filename->getClientOriginalExtension();
-        //     $request()->file('filename')->storeAs('filename',$user->id.'/','');
-        //     $user->update(['filename'->$filename]);
-        //     Storage::disk('public')->put($filename->getFilename().'.'.$extension,  File::get($filename));
-        //     $store->save();
+       
+        if ($request->file('filename')) {
+            $files = $request->file('filename');
+            $destinationPath = 'public/image/'; // upload path
+            $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
+            $files->move($destinationPath, $profileImage);
+        }
         
         $store = new Store();
-        
-        $store->name=$request->get('name');
-        $store->store_name=$request->get('store_name');	
-        $store->email =$request->get('email');		
-		$store->password=$request->get('password');	
-        $store->phone =$request->get('phone');	
-        $store->store_address=$request->get('store_address');		
-        // $store->filename=$request->get('filename');
+        $store->name = $request->get('name');
+        $store->store_name = $request->get('store_name');	
+        $store->email = $request->get('email');		
+		$store->password = $request->get('password');	
+        $store->phone = $request->get('phone');	
+        $store->store_address = $request->get('store_address');		
+        $store->filename = $profileImage;
         // $request->file->storeAs('filename', $request->file->getClientOriginalName(),'');				
         $store->save();
         return redirect()->route('store.index')->withStatus(__('User successfully created.'));
-
-        request()->validate([
-            'filename' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-       ]);
-       if ($files = $request->file('filename')) {
-           $destinationPath = 'public/'; // upload path
-           $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
-           $files->move($destinationPath, $profileImage);
-        }
-
-
-
         
-      }
     }
+}
