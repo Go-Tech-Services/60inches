588683376080aaee9c89296941c3abf3dddd645e
deepalidsp <dipali.kolhe51@gmail.com>
2021-01-05 15:55:02 +0530

    input comment

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Support\Facades\Input::class,
+        // Illuminate\Support\Facades\Input::class,
 
 
         /*
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Input' => Illuminate\Support\Facades\Input::class,
+        // 'Input' => Illuminate\Support\Facades\Input::class,
     ],
 
 ];
