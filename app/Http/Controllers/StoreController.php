58114cafa833ae19c79ca90745ff9c8de02ae631
deepalidsp <dipali.kolhe51@gmail.com>
2021-01-07 21:58:05 +0530

    conflict

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -37,32 +37,6 @@ class StoreController extends Controller
         // $validator = \Validator::make($request->all(), $rules);
         // dd($request);  
        $request->validate([
-<<<<<<< HEAD
-                    'owner_name' => 'required',
-                    'store_name' => 'required', 
-                    'email' => 'required', 
-                    'store_address' => 'required', 
-                    'phone' => 'required',
-                    'store_logo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-                ]);
-        $profileImage = '';
-        if ($request->file('store_logo')) {
-            $files = $request->file('store_logo');
-            $destinationPath = 'public/image/'; // upload path
-            $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
-            $files->move($destinationPath, $profileImage);
-        }
-        $store = DB::table('store_info')->select('*');
-        return datatables()->of($stores)
-            ->make(true);
-        // $project = Store::create($data);
-        // $store = new Store();
-        // $store->owner_name = $request->get('owner_name');
-        // $store->store_name = $request->get('store_name');	
-        // $store->email = $request->get('email');
-        // $store->store_address = $request->get('store_address');		
-        // $store->phone = $request->get('phone');	
-=======
             'owner_name' => 'required',
             'store_name' => 'required', 
             'email' => 'required', 
@@ -86,7 +60,6 @@ class StoreController extends Controller
         $store->email = $request->get('email');
         $store->store_address = $request->get('store_address');		
         $store->phone = $request->get('phone');	
->>>>>>> eb470e34556b48f86c2ed7466733de5c86672dc0
         // $store->store_logo = $profileImage ?? '';
         $store->store_logo = $request->get('store_logo');
         $store->created_by = \Auth::user()->id;
