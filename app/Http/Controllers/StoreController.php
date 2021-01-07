eb470e34556b48f86c2ed7466733de5c86672dc0
deepalidsp <dipali.kolhe51@gmail.com>
2021-01-07 16:55:58 +0530

    store store

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -20,6 +20,7 @@ class StoreController extends Controller
         return view('store.create');
     }
     public function store(Request $request){
+        // dd($request);
         // dd(\Auth::user()->id);
         // $rules = array(
         //     'owner_name' => 'required',
@@ -33,31 +34,34 @@ class StoreController extends Controller
         // $validator = \Validator::make($request->all(), $rules);
         // dd($request);  
        $request->validate([
-                    'owner_name' => 'required',
-                    'store_name' => 'required', 
-                    'email' => 'required', 
-                    'store_address' => 'required', 
-                    'phone' => 'required',
-                    'store_logo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-                ]);
-        $profileImage = '';
-        if ($request->file('store_logo')) {
-            $files = $request->file('store_logo');
-            $destinationPath = 'public/image/'; // upload path
-            $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
-            $files->move($destinationPath, $profileImage);
-        }
-        $project = Store::create($data);
-        // $store = new Store();
-        // $store->owner_name = $request->get('owner_name');
-        // $store->store_name = $request->get('store_name');	
-        // $store->email = $request->get('email');
-        // $store->store_address = $request->get('store_address');		
-        // $store->phone = $request->get('phone');	
+            'owner_name' => 'required',
+            'store_name' => 'required', 
+            'email' => 'required', 
+            'store_address' => 'required', 
+            'phone' => 'required',
+            'store_logo' => 'required',
+            // 'store_logo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        // $profileImage = '';
+        // if ($request->store_logo) {
+        //     $files = $request->store_logo;
+        //     $destinationPath = 'public/image/'; // upload path
+        //     $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
+        //     $files->move($destinationPath, $profileImage);
+        // }
+        // dd($profileImage);
+        // $project = Store::create($data);
+        $store = new Store();
+        $store->owner_name = $request->get('owner_name');
+        $store->store_name = $request->get('store_name');	
+        $store->email = $request->get('email');
+        $store->store_address = $request->get('store_address');		
+        $store->phone = $request->get('phone');	
         // $store->store_logo = $profileImage ?? '';
-        // $store->created_by = \Auth::user()->id;
-        // $store->updated_by = null;// $request->file->storeAs('filename', $request->file->getClientOriginalName(),'');				
-        // $store->save();
+        $store->store_logo = $request->get('store_logo');
+        $store->created_by = \Auth::user()->id;
+        $store->updated_by = null;// $request->file->storeAs('filename', $request->file->getClientOriginalName(),'');				
+        $store->save();
         return redirect()->route('store.index')->withStatus(__('User successfully created.'));
         
     }
